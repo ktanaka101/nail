78703f9e2335a2c964d1c206f185403a2d0720e9
ktanaka101 <kentanaka101@gmail.com>
2020-08-09 13:04:46 +0900

    Update addition of numbers by LLVM

--- src/llvm/compiler.rs
+++ src/llvm/compiler.rs
@@ -1,16 +1,16 @@
+use anyhow::Result;
+use std::convert::TryInto;
+
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::execution_engine::{ExecutionEngine, JitFunction};
 use inkwell::module::Module;
-use inkwell::targets::{InitializationConfig, Target};
+use inkwell::values;
 use inkwell::OptimizationLevel;
-use std::error::Error;
 
-/// Convenience type alias for the `sum` function.
-///
-/// Calling this is innately `unsafe` because there's no guarantee it doesn't
-/// do `unsafe` operations internally.
-type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
+use crate::parser::ast;
+
+type MainFunc = unsafe extern "C" fn() -> i64;
 
 pub struct Compiler<'ctx> {
     context: &'ctx Context,
@@ -21,7 +21,7 @@ pub struct Compiler<'ctx> {
 
 impl<'ctx> Compiler<'ctx> {
     pub fn new(context: &'ctx Context) -> Self {
-        let module = context.create_module("sum");
+        let module = context.create_module("top");
         let execution_engine = module
             .create_jit_execution_engine(OptimizationLevel::None)
             .unwrap();
@@ -34,29 +34,63 @@ impl<'ctx> Compiler<'ctx> {
         }
     }
 
-    pub fn compile(&self) -> Result<JitFunction<SumFunc>, Box<dyn Error>> {
-        Ok(self
-            .jit_compile_sum()
-            .ok_or("Unable to JIT compile `sum`")?)
-    }
-
-    fn jit_compile_sum(&self) -> Option<JitFunction<SumFunc>> {
-        let i64_type = self.context.i64_type();
-        let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
-        let function = self.module.add_function("sum", fn_type, None);
-        let basic_block = self.context.append_basic_block(function, "entry");
+    pub fn compile(&self, node: &ast::Node) -> Result<JitFunction<MainFunc>> {
+        let fn_type = self.context.void_type().fn_type(&[], false);
+        let main_fn = self.module.add_function("main", fn_type, None);
+        let basic_block = self.context.append_basic_block(main_fn, "entry");
 
         self.builder.position_at_end(basic_block);
 
-        let x = function.get_nth_param(0)?.into_int_value();
-        let y = function.get_nth_param(1)?.into_int_value();
-        let z = function.get_nth_param(2)?.into_int_value();
+        println!("{:?}", node);
 
-        let sum = self.builder.build_int_add(x, y, "sum");
-        let sum = self.builder.build_int_add(sum, z, "sum");
+        match self.compile_node(node)? {
+            Some(v) => self.builder.build_return(Some(&v)),
+            None => self.builder.build_return(None),
+        };
 
-        self.builder.build_return(Some(&sum));
+        self.module.print_to_stderr();
 
-        unsafe { self.execution_engine.get_function("sum").ok() }
+        Ok(unsafe { self.execution_engine.get_function("main")? })
+    }
+
+    fn compile_node(&self, node: &ast::Node) -> Result<Option<values::IntValue>> {
+        Ok(match node {
+            ast::Node::Expr(expr) => Some(self.compile_expr(expr)?),
+            ast::Node::Stmt(stmt) => Some(self.compile_stmt(stmt)?),
+            ast::Node::Program(pg) => {
+                let mut last: Option<values::IntValue> = None;
+                pg.statements.iter().try_for_each::<_, Result<()>>(|stmt| {
+                    last = Some(self.compile_stmt(stmt)?);
+                    Ok(())
+                })?;
+
+                last
+            }
+        })
+    }
+
+    fn compile_stmt(&self, stmt: &ast::Stmt) -> Result<values::IntValue> {
+        Ok(match stmt {
+            ast::Stmt::ExprStmt(expr_stmt) => self.compile_expr(&expr_stmt.expr)?,
+            _ => unimplemented!(),
+        })
+    }
+
+    fn compile_expr(&self, expr: &ast::Expr) -> Result<values::IntValue> {
+        Ok(match expr {
+            ast::Expr::Integer(int) => self
+                .context
+                .i64_type()
+                .const_int(int.value.try_into().unwrap(), false),
+            ast::Expr::InfixExpr(infix_expr) => {
+                let lvalue = self.compile_expr(&*infix_expr.left)?;
+                let rvalue = self.compile_expr(&*infix_expr.right)?;
+                match infix_expr.ope {
+                    ast::Operator::Plus => self.builder.build_int_add(lvalue, rvalue, "sum"),
+                    _ => unimplemented!(),
+                }
+            }
+            _ => unimplemented!(),
+        })
     }
 }

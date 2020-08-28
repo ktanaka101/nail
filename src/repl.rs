65dbf53dcf9586bd78b668c03f71e287a64b4b88
ktanaka101 <kentanaka101@gmail.com>
2020-08-28 22:06:10 +0900

    Use tty for repl

--- src/repl.rs
+++ src/repl.rs
@@ -7,6 +7,9 @@ use std::rc::Rc;
 
 use inkwell::context::Context;
 use inkwell::OptimizationLevel;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 use crate::evaluator::env::Environment;
 use crate::evaluator::object;
@@ -18,19 +21,31 @@ use crate::parser::Parser;
 
 const PROMPT: &str = ">> ";
 
+pub enum Interface {
+    TTY,
+    STD,
+}
+
 pub enum Executer {
     Evaluator,
-    LLVM,
+    LLVM(Interface),
 }
 
 pub fn start(executer: Executer) {
     match executer {
         Executer::Evaluator => start_evaluator(),
-        Executer::LLVM => start_llvm(),
+        Executer::LLVM(interface) => start_llvm(interface),
     }
 }
 
-fn start_llvm() {
+fn start_llvm(interface: Interface) {
+    match interface {
+        Interface::STD => start_llvm_on_std(),
+        Interface::TTY => start_llvm_on_tty(),
+    }
+}
+
+fn start_llvm_on_std() {
     let mut inputs = "".to_string();
 
     loop {
@@ -85,6 +100,136 @@ fn start_llvm() {
         println!("{}", result_string);
 
         inputs = try_inputs.clone();
+    }
+}
+
+fn start_llvm_on_tty() {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+
+    let mut inputs = "".to_string();
+    write!(
+        stdout,
+        "{}{}>> ",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+    )
+    .unwrap();
+
+    stdout.flush().unwrap();
+    let mut row = 1;
+
+    let mut line = "".to_string();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => {
+                write!(stdout, "q").unwrap();
+                break;
+            }
+            Key::Char(c) => match c {
+                '\n' => {
+                    if line.is_empty() {
+                        row += 1;
+                        write!(stdout, "{}>> ", termion::cursor::Goto(1, row)).unwrap();
+                        stdout.flush().unwrap();
+                        continue;
+                    }
+
+                    let context = Context::create();
+                    let module = context.create_module("top");
+                    let builder = context.create_builder();
+                    let execution_engine = module
+                        .create_jit_execution_engine(OptimizationLevel::None)
+                        .unwrap();
+
+                    let mut compiler =
+                        Compiler::new(&context, &module, &builder, &execution_engine);
+
+                    let mut try_inputs = inputs.clone();
+                    try_inputs.push('\n');
+
+                    line.push(';');
+                    try_inputs.push_str(&line);
+                    line = "".to_string();
+
+                    let lexer = Lexer::new(try_inputs.clone());
+                    let mut parser = Parser::new(lexer);
+
+                    let program = match parser.parse_program() {
+                        Ok(p) => p,
+                        Err(e) => {
+                            row += 1;
+                            write!(
+                                stdout,
+                                "{}Parse error: {}",
+                                termion::cursor::Goto(1, row),
+                                e
+                            )
+                            .unwrap();
+                            row += 1;
+                            write!(stdout, "{}>> ", termion::cursor::Goto(1, row)).unwrap();
+                            stdout.flush().unwrap();
+                            continue;
+                        }
+                    };
+
+                    let main_fn = match compiler.compile(
+                        &program.into(),
+                        false,
+                        compiler::Output::CStringPtr,
+                    ) {
+                        Ok(f) => f,
+                        Err(e) => {
+                            row += 1;
+                            write!(stdout, "{}LLVM error: {}", termion::cursor::Goto(1, row), e)
+                                .unwrap();
+                            row += 1;
+                            write!(stdout, "{}>> ", termion::cursor::Goto(1, row)).unwrap();
+                            stdout.flush().unwrap();
+                            continue;
+                        }
+                    };
+
+                    let result_string = {
+                        let c_string_ptr = unsafe { main_fn.call() };
+                        unsafe { CString::from_raw(c_string_ptr) }
+                            .into_string()
+                            .unwrap()
+                    };
+
+                    row += 1;
+                    write!(stdout, "{}{}", termion::cursor::Goto(1, row), result_string).unwrap();
+
+                    row += 1;
+                    write!(stdout, "{}>> ", termion::cursor::Goto(1, row)).unwrap();
+
+                    inputs = try_inputs.clone();
+                }
+                c => {
+                    line.push(c);
+                    let line_offset: u16 = line.len().try_into().unwrap();
+                    let offset = line_offset + 4;
+                    write!(
+                        stdout,
+                        "{}>> {}{}",
+                        termion::cursor::Goto(1, row),
+                        line,
+                        termion::cursor::Goto(offset, row)
+                    )
+                    .unwrap();
+                }
+            },
+            Key::Alt(c) => println!("Alt-{}", c),
+            Key::Ctrl(c) => println!("Ctrl-{}", c),
+            Key::Left => println!("<left>"),
+            Key::Right => println!("<right>"),
+            Key::Up => println!("<up>"),
+            Key::Down => println!("<down>"),
+            _ => println!("Other"),
+        }
+
+        stdout.flush().unwrap();
     }
 }
 

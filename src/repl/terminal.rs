e1e2cf5a04f24e0b65703aab82de8d777d896859
ktanaka101 <kentanaka101@gmail.com>
2020-08-30 00:25:22 +0900

    Refactoring

--- src/repl/terminal.rs
+++ src/repl/terminal.rs
@@ -5,16 +5,41 @@ use std::io::Write;
 use termion;
 
 pub struct Terminal {
+    pub cursor_pos: Pos,
+    stdout: termion::raw::RawTerminal<io::Stdout>,
+}
+
+pub struct Pos {
     row: u16,
     col: u16,
-    stdout: termion::raw::RawTerminal<io::Stdout>,
+}
+
+impl Pos {
+    fn right(&mut self, num: u16) {
+        self.col += num;
+    }
+
+    fn left(&mut self, num: u16) {
+        self.col -= num;
+    }
+
+    fn up(&mut self, num: u16) {
+        self.row -= num;
+    }
+
+    fn down(&mut self, num: u16) {
+        self.row += num;
+    }
+}
+
+pub enum MoveCursorAction {
+    Left(u16),
 }
 
 impl Terminal {
     pub fn new(stdout: termion::raw::RawTerminal<io::Stdout>) -> Terminal {
         Self {
-            row: 0,
-            col: 0,
+            cursor_pos: Pos { row: 0, col: 0 },
             stdout,
         }
     }
@@ -24,22 +49,24 @@ impl Terminal {
     }
 
     pub fn clear(&mut self) {
-        self.row = 1;
-        self.col = 1;
+        self.cursor_pos.col = 1;
+        self.cursor_pos.row = 1;
+
         write!(
             self.stdout,
             "{}{}",
             termion::clear::All,
-            termion::cursor::Goto(self.col, self.row)
+            self.get_goto_current_pos()
         )
         .unwrap();
         self.stdout.flush().unwrap();
     }
 
     pub fn next_line(&mut self) {
-        self.col = 1;
-        self.row += 1;
-        write!(self.stdout, "{}", termion::cursor::Goto(self.col, self.row)).unwrap();
+        self.cursor_pos.col = 1;
+        self.cursor_pos.down(1);
+
+        write!(self.stdout, "{}", self.get_goto_current_pos()).unwrap();
         self.stdout.flush().unwrap();
     }
 
@@ -47,6 +74,10 @@ impl Terminal {
         write!(self.stdout, "{}", string).unwrap();
         self.stdout.flush().unwrap();
         let len: u16 = string.len().try_into().unwrap();
-        self.col += len;
+        self.cursor_pos.right(len);
+    }
+
+    fn get_goto_current_pos(&self) -> termion::cursor::Goto {
+        termion::cursor::Goto(self.cursor_pos.col, self.cursor_pos.row)
     }
 }

880c37268d444d4666777fd0ce538279f63b7228
ktanaka101 <kentanaka101@gmail.com>
2020-12-08 00:20:00 +0900

    Update build utils

--- run_it.sh
+++ run_it.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 SRC_DIR=`pwd`
 
-docker run --rm -it -v ${SRC_DIR}:/usr/src/nail -w /usr/src/nail ktanaka101/rust-and-llvm10-on-ubuntu18.04:latest
+docker run --rm -it -v ${SRC_DIR}:/usr/src/nail -w /usr/src/nail ktanaka101/nail-builder:latest
--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 SRC_DIR=`pwd`
 
-docker run --rm -v ${SRC_DIR}:/usr/src/nail -w /usr/src/nail ktanaka101/rust-and-llvm10-on-ubuntu18.04:latest cargo test
+docker run --rm -v ${SRC_DIR}:/usr/src/nail -w /usr/src/nail ktanaka101/nail-builder:latest cargo test

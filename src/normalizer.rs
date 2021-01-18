b14ec4229da1ab11647c7446e64ba969330b30a0
ktanaka101 <kentanaka101@gmail.com>
2021-01-18 20:56:06 +0900

    Fix clippy warnings

--- src/normalizer.rs
+++ src/normalizer.rs
@@ -1,2 +1,19 @@
-mod normalizer;
-pub use normalizer::Normalizer;
+use anyhow::Result;
+
+use crate::parser::ast;
+
+#[derive(thiserror::Error, Debug, Clone)]
+pub enum Error {}
+
+#[derive(Default)]
+pub struct Normalizer {}
+
+impl Normalizer {
+    pub fn new() -> Self {
+        Normalizer {}
+    }
+
+    pub fn normalize(&self, node: &ast::Node) -> Result<ast::Node> {
+        Ok(node.clone())
+    }
+}

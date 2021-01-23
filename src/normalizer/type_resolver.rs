a1cba6efe934889ffb2f07b6a6943fded13a388b
ktanaka101 <kentanaka101@gmail.com>
2021-01-23 23:44:59 +0900

    Add tests

--- src/normalizer/type_resolver.rs
+++ src/normalizer/type_resolver.rs
@@ -60,3 +60,57 @@ impl TypeResolver for ast::StringLit {
         Some(ast::Type::String)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn resolve_type_by_array() {
+        let arr = ast::Array { elements: vec![] };
+        assert_eq!(arr.resolve_type(), Some(ast::Type::Array));
+    }
+
+    #[test]
+    fn resolve_type_by_boolean() {
+        let boolean = ast::Boolean { value: false };
+        assert_eq!(boolean.resolve_type(), Some(ast::Type::Boolean));
+    }
+
+    #[test]
+    fn resolve_type_by_function() {
+        let f = ast::Function {
+            fn_type: ast::FunctionType::Function,
+            params: vec![],
+            name: "".into(),
+            body: ast::Block { statements: vec![] },
+        };
+        assert_eq!(f.resolve_type(), Some(ast::Type::Function));
+    }
+
+    #[test]
+    fn resolve_type_by_hash() {
+        let hs = ast::Hash { pairs: vec![] };
+        assert_eq!(hs.resolve_type(), Some(ast::Type::Hash));
+    }
+
+    #[test]
+    fn resolve_type_by_integer() {
+        let int = ast::Integer { value: 0 };
+        assert_eq!(int.resolve_type(), Some(ast::Type::Integer));
+    }
+
+    #[test]
+    fn resolve_type_by_char() {
+        let c = ast::Char {
+            value: 'a'.to_string(),
+        };
+        assert_eq!(c.resolve_type(), Some(ast::Type::Char));
+    }
+
+    #[test]
+    fn resolve_type_by_string() {
+        let string = ast::StringLit { value: "".into() };
+        assert_eq!(string.resolve_type(), Some(ast::Type::String));
+    }
+}

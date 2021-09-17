81ca1538c60cb9ceb2f5d18bde4a4b85380b94db
ktanaka101 <kentanaka101@gmail.com>
2021-09-18 02:17:50 +0900

    Delete files that are no longer needed by switching to dev-container

--- run_it.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-SRC_DIR=`pwd`
-
-docker run --rm -it -v ${SRC_DIR}:/usr/src/nail -w /usr/src/nail ktanaka101/nail-builder:latest
--- run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd `dirname $0`
-
-SRC_DIR=`pwd`
-
-docker run --rm -v ${SRC_DIR}:/usr/src/nail -w /usr/src/nail ktanaka101/nail-builder:latest cargo test

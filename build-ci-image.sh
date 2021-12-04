f7e0e446486f1d1ffc973aa2cfb941ddd7793cab
ktanaka101 <kentanaka101@gmail.com>
2021-12-04 21:43:16 +0900

    Add arguments to the building shell file

--- build-ci-image.sh
+++ build-ci-image.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-docker buildx build --platform linux/arm64,linux/amd64 --target ci -t $1 .
+tag_name=""
+should_push=""
+
+usage() {
+  echo "Usage: $0 [-t tagname] [-p(push)]"
+}
+
+while getopts pt:h OPT
+do
+  case $OPT in
+    "t" ) tag_name=$OPTARG ;;
+    "p" ) should_push=true ;;
+    "h" ) usage; exit 1 ;;
+  esac
+done
+
+cmd_args=""
+if [ "$tag_name" = "" ]; then
+  echo "Please set '-t TAG_NAME'"
+  exit 1
+else
+  cmd_args+="-t $tag_name"
+fi
+
+if "$should_push"; then
+  cmd_args+=" --push"
+fi
+
+docker buildx build --platform linux/arm64,linux/amd64 --target ci $cmd_args .

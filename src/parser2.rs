327f5edbbf44caefb8da7ebf205991726c16b35a
ktanaka101 <kentanaka101@gmail.com>
2022-10-31 15:00:01 +0900

    Update parser2

--- src/parser2.rs
+++ src/parser2.rs
@@ -16,16 +16,40 @@ impl<'a> Parser<'a> {
     }
 
     pub(crate) fn parse(mut self) -> Parse {
-        self.builder
-            .start_node(NailLanguage::kind_to_raw(SyntaxKind::Root));
-        self.builder.finish_node();
+        self.start_node(SyntaxKind::Root);
+        self.finish_node();
 
         Parse {
             green_node: self.builder.finish(),
         }
     }
+
+    fn start_node(&mut self, kind: SyntaxKind) {
+        self.builder.start_node(NailLanguage::kind_to_raw(kind));
+    }
+
+    fn finish_node(&mut self) {
+        self.builder.finish_node();
+    }
 }
 
 pub(crate) struct Parse {
     green_node: GreenNode,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::syntax::SyntaxNode;
+
+    #[test]
+    fn parse_nothing() {
+        let parse = Parser::new("").parse();
+
+        assert_eq!(
+            format!("{:#?}", SyntaxNode::new_root(parse.green_node)),
+            r#"Root@0..0
+"#,
+        );
+    }
 }

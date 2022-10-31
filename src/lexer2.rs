7c3ad709254e22da51da1c87309af36bb91f5994
ktanaka101 <kentanaka101@gmail.com>
2022-10-31 15:32:02 +0900

    Update lexer2

--- src/lexer2.rs
+++ src/lexer2.rs
@@ -84,15 +84,36 @@ pub(crate) enum SyntaxKind {
     Error,
 }
 
+pub(crate) struct Lexer<'a> {
+    inner: logos::Lexer<'a, SyntaxKind>,
+}
+
+impl<'a> Lexer<'a> {
+    pub(crate) fn new(input: &'a str) -> Self {
+        Self {
+            inner: SyntaxKind::lexer(input),
+        }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = (SyntaxKind, &'a str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let kind = self.inner.next()?;
+        let text = self.inner.slice();
+
+        Some((kind, text))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     fn check(input: &str, kind: SyntaxKind) {
-        let mut lexer = SyntaxKind::lexer(input);
-
-        assert_eq!(lexer.next(), Some(kind));
-        assert_eq!(lexer.slice(), input);
+        let mut lexer = Lexer::new(input);
+        assert_eq!(lexer.next(), Some((kind, input)));
     }
 
     #[test]

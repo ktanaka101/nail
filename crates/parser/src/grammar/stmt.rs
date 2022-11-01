f30e2a2e125c8e6978b1ead91b9462a6172e47a1
ktanaka101 <kentanaka101@gmail.com>
2022-11-01 19:56:03 +0900

    Update

--- crates/parser/src/grammar/stmt.rs
+++ crates/parser/src/grammar/stmt.rs
@@ -67,4 +67,25 @@ mod tests {
                       Ident@16..17 "a""#]],
         );
     }
+
+    #[test]
+    fn parse_multiple_statements() {
+        check(
+            "let a = 1\na",
+            expect![[r#"
+                Root@0..11
+                  VariableDef@0..10
+                    LetKw@0..3 "let"
+                    Whitespace@3..4 " "
+                    Ident@4..5 "a"
+                    Whitespace@5..6 " "
+                    Equals@6..7 "="
+                    Whitespace@7..8 " "
+                    Literal@8..10
+                      IntegerLiteral@8..9 "1"
+                      Whitespace@9..10 "\n"
+                  VariableRef@10..11
+                    Ident@10..11 "a""#]],
+        );
+    }
 }

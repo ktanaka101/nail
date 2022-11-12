518b2e012e90e1e2d639443a49dd81ed33fa0b3f
ktanaka101 <kentanaka101@gmail.com>
2022-11-12 15:17:11 +0000

    wip

--- crates/lsp/src/lib.rs
+++ crates/lsp/src/lib.rs
@@ -3,11 +3,13 @@ mod line_index;
 
 use std::sync::Arc;
 
+use text_size::TextSize;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
 use context::{Analysis, Context};
+use syntax::SyntaxKind;
 
 pub async fn run_server() {
     let stdin = tokio::io::stdin();
@@ -77,6 +79,13 @@ const SEMANTIC_TOKEN_TYPES: &[SemanticTokenType] = &[
     SemanticTokenType::VARIABLE,
     SemanticTokenType::OPERATOR,
 ];
+
+fn raw_semantic_token(token_type: SemanticTokenType) -> u32 {
+    SEMANTIC_TOKEN_TYPES
+        .iter()
+        .position(|t| *t == token_type)
+        .unwrap() as u32
+}
 
 impl Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
@@ -184,6 +193,12 @@ impl Backend {
         &self,
         params: SemanticTokensParams,
     ) -> Result<Option<SemanticTokensResult>> {
+        self.info(&format!(
+            "server semantic tokens full! get uri: {}",
+            params.text_document.uri
+        ))
+        .await;
+
         if let Some(analysis) = self.context.get_analysis_from_cache(params.text_document) {
             let tokens = get_semantic_tokens(analysis);
             Ok(Some(SemanticTokensResult::Tokens(SemanticTokens {
@@ -201,7 +216,115 @@ impl Backend {
 }
 
 fn get_semantic_tokens(analysis: &Analysis) -> Vec<SemanticToken> {
-    todo!()
+    let mut prev_line = TextSize::from(0);
+    let mut prev_col = TextSize::from(0);
+
+    let mut tokens = vec![];
+    let line_index = &analysis.line_index;
+
+    for event in analysis.parsed.syntax().preorder_with_tokens() {
+        use rowan::WalkEvent;
+        match event {
+            WalkEvent::Enter(node) => match node.kind() {
+                SyntaxKind::LetKw => {
+                    let range = node.text_range();
+                    let pos = line_index.line_col(range.start());
+                    let line = pos.line_number().0;
+                    let col = pos.col_number().0;
+                    let delta_line: u32 = (line - prev_line).into();
+                    let delta_start: u32 = if delta_line == 0 {
+                        (col - prev_col).into()
+                    } else {
+                        col.into()
+                    };
+
+                    let token = SemanticToken {
+                        delta_line,
+                        delta_start,
+                        length: range.len().into(),
+                        token_type: raw_semantic_token(SemanticTokenType::KEYWORD),
+                        token_modifiers_bitset: 0,
+                    };
+                    prev_line = line;
+                    prev_col = col;
+                    tokens.push(token);
+                }
+                SyntaxKind::Ident => {
+                    let range = node.text_range();
+                    let pos = line_index.line_col(range.start());
+                    let line = pos.line_number().0;
+                    let col = pos.col_number().0;
+                    let delta_line: u32 = (line - prev_line).into();
+                    let delta_start: u32 = if delta_line == 0 {
+                        (col - prev_col).into()
+                    } else {
+                        col.into()
+                    };
+
+                    let token = SemanticToken {
+                        delta_line,
+                        delta_start,
+                        length: range.len().into(),
+                        token_type: raw_semantic_token(SemanticTokenType::VARIABLE),
+                        token_modifiers_bitset: 0,
+                    };
+                    prev_line = line;
+                    prev_col = col;
+                    tokens.push(token);
+                }
+                SyntaxKind::Eq => {
+                    let range = node.text_range();
+                    let pos = line_index.line_col(range.start());
+                    let line = pos.line_number().0;
+                    let col = pos.col_number().0;
+                    let delta_line: u32 = (line - prev_line).into();
+                    let delta_start: u32 = if delta_line == 0 {
+                        (col - prev_col).into()
+                    } else {
+                        col.into()
+                    };
+
+                    let token = SemanticToken {
+                        delta_line,
+                        delta_start,
+                        length: range.len().into(),
+                        token_type: raw_semantic_token(SemanticTokenType::OPERATOR),
+                        token_modifiers_bitset: 0,
+                    };
+                    prev_line = line;
+                    prev_col = col;
+                    tokens.push(token);
+                }
+                SyntaxKind::IntegerLiteral => {
+                    let range = node.text_range();
+                    let pos = line_index.line_col(range.start());
+                    let line = pos.line_number().0;
+                    let col = pos.col_number().0;
+                    let delta_line: u32 = (line - prev_line).into();
+                    let delta_start: u32 = if delta_line == 0 {
+                        (col - prev_col).into()
+                    } else {
+                        col.into()
+                    };
+
+                    let token = SemanticToken {
+                        delta_line,
+                        delta_start,
+                        length: range.len().into(),
+                        token_type: raw_semantic_token(SemanticTokenType::NUMBER),
+                        token_modifiers_bitset: 0,
+                    };
+                    prev_line = line;
+                    prev_col = col;
+                    tokens.push(token);
+                }
+                _ => (),
+            },
+            WalkEvent::Leave(_) => (),
+        }
+    }
+
+    tokens
 }
 
 fn get_diagnostics(analysis: &Analysis) -> Vec<Diagnostic> {
@@ -215,4 +338,131 @@ fn get_diagnostics(analysis: &Analysis) -> Vec<Diagnostic> {
             )
         })
         .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check(text: String, expected: Vec<SemanticToken>) {
+        let url = Url::parse("https://example.net/a/b.html").unwrap();
+        let analysis = Analysis::new(url, text);
+        let tokens = get_semantic_tokens(&analysis);
+
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_get_semantic_tokens() {
+        check(
+            "let a = 10".to_string(),
+            vec![
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 0,
+                    length: 3,
+                    token_type: raw_semantic_token(SemanticTokenType::KEYWORD),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 4,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::VARIABLE),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 2,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::OPERATOR),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 2,
+                    length: 2,
+                    token_type: raw_semantic_token(SemanticTokenType::NUMBER),
+                    token_modifiers_bitset: 0,
+                },
+            ],
+        );
+    }
+
+    #[test]
+    fn test_get_semantic_tokens_with_newline() {
+        check(
+            r#"
+let a = 1
+let b = 10
+  100
+"#
+            .to_string(),
+            vec![
+                SemanticToken {
+                    delta_line: 1,
+                    delta_start: 0,
+                    length: 3,
+                    token_type: raw_semantic_token(SemanticTokenType::KEYWORD),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 4,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::VARIABLE),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 2,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::OPERATOR),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 2,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::NUMBER),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 1,
+                    delta_start: 0,
+                    length: 3,
+                    token_type: raw_semantic_token(SemanticTokenType::KEYWORD),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 4,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::VARIABLE),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 2,
+                    length: 1,
+                    token_type: raw_semantic_token(SemanticTokenType::OPERATOR),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 0,
+                    delta_start: 2,
+                    length: 2,
+                    token_type: raw_semantic_token(SemanticTokenType::NUMBER),
+                    token_modifiers_bitset: 0,
+                },
+                SemanticToken {
+                    delta_line: 1,
+                    delta_start: 2,
+                    length: 3,
+                    token_type: raw_semantic_token(SemanticTokenType::NUMBER),
+                    token_modifiers_bitset: 0,
+                },
+            ],
+        );
+    }
 }

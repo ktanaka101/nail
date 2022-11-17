a9c87ad60eedade083396d985a532b1bf1d926ca
ktanaka101 <kentanaka101@gmail.com>
2022-11-17 14:11:59 +0000

    wip

--- crates/hir_ty/src/lib.rs
+++ crates/hir_ty/src/lib.rs
@@ -1,1 +1,15 @@
+#[derive(Debug, Clone)]
+pub struct InferenceResult {}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ResolvedType {
+    Unknown,
+    Integer,
+    String,
+    Char,
+    Bool,
+}
+
+fn infer_body(body: Vec<hir::Stmt>, database: &hir::Database) -> InferenceResult {
+    todo!()
+}

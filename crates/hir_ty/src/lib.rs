61bbc791dac1c46acaa12b08fa8a845029616bb6
ktanaka101 <kentanaka101@gmail.com>
2022-11-18 17:47:57 +0900

    wip

--- crates/hir_ty/src/lib.rs
+++ crates/hir_ty/src/lib.rs
@@ -1,5 +1,32 @@
+use std::collections;
+
+#[derive(Debug)]
+pub struct InferenceResult {
+    pub mapping: collections::HashMap<hir::ExprIdx, ResolvedType>,
+    pub errors: Vec<InferenceError>,
+}
+
+impl InferenceResult {
+    fn new() -> Self {
+        Self {
+            mapping: collections::HashMap::new(),
+            errors: Vec::new(),
+        }
+    }
+
+    #[cfg(test)]
+    fn debug(&self) -> String {
+        let mut msg = "".to_string();
+        for (expr, ty) in &self.mapping {
+            msg.push_str(&format!("{:?}: {:?}\n", expr.into_raw(), ty));
+        }
+
+        msg
+    }
+}
+
 #[derive(Debug, Clone)]
-pub struct InferenceResult {}
+pub enum InferenceError {}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ResolvedType {
@@ -10,6 +37,86 @@ pub enum ResolvedType {
     Bool,
 }
 
-fn infer_body(body: Vec<hir::Stmt>, database: &hir::Database) -> InferenceResult {
-    todo!()
+pub fn infer_body(body: Vec<hir::Stmt>, database: &hir::Database) -> InferenceResult {
+    let mut inference_result = InferenceResult::new();
+
+    for stmt in body {
+        match stmt {
+            hir::Stmt::Expr(expr) => {
+                let ty = infer_expr(&database.exprs[expr]);
+                inference_result.mapping.insert(expr, ty);
+            }
+            hir::Stmt::VariableDef { value, .. } => {
+                let ty = infer_expr(&database.exprs[value]);
+                inference_result.mapping.insert(value, ty);
+            }
+        }
+    }
+
+    inference_result
+}
+
+fn infer_expr(expr: &hir::Expr) -> ResolvedType {
+    match expr {
+        hir::Expr::Literal(literal) => match literal {
+            hir::Literal::Bool(_) => ResolvedType::Bool,
+            _ => todo!(),
+        },
+        _ => todo!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use expect_test::{expect, Expect};
+
+    fn check(input: &str, expect: Expect) {
+        let parsed = parser::parse(input);
+        let ast = ast::SourceFile::cast(parsed.syntax()).unwrap();
+        let (db, body) = hir::lower(ast);
+        let result = infer_body(body, &db);
+        expect.assert_eq(&result.debug());
+    }
+
+    #[test]
+    fn infer_bool_test() {
+        check(
+            "true",
+            expect![[r#"
+                0: Bool
+            "#]],
+        );
+
+        check(
+            "false",
+            expect![[r#"
+                0: Bool
+            "#]],
+        );
+    }
+
+    #[test]
+    fn infer_variable_def() {
+        check(
+            "let a = true",
+            expect![[r#"
+                0: Bool
+            "#]],
+        )
+    }
+
+    #[test]
+    fn infer_multiline_variable_def() {
+        check(
+            r#"
+                let a = true
+                let b = false
+            "#,
+            expect![[r#"
+                0: Bool
+                1: Bool
+            "#]],
+        )
+    }
 }

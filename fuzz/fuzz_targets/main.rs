875131af7d925dbce25cc162528b72b1f71747fe
ktanaka101 <kentanaka101@gmail.com>
2022-11-27 04:51:15 +0000

    wip

--- fuzz/fuzz_targets/main.rs
+++ fuzz/fuzz_targets/main.rs
@@ -2,12 +2,14 @@
 
 use libfuzzer_sys::fuzz_target;
 
+use ast::AstNode;
+
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let parse = parser::parse(s);
         let syntax = parse.syntax();
         let _validation_errors = ast::validation::validate(&syntax);
         let source_file = ast::SourceFile::cast(syntax).unwrap();
-        let (_database, _stmts) = hir::lower(source_file);
+        let (_, ..) = hir::lower(source_file);
     }
 });

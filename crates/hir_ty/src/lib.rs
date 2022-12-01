b088914d7a911bbdbf69180979de3b51791df226
ktanaka101 <kentanaka101@gmail.com>
2022-12-01 14:04:36 +0000

    wip

--- crates/hir_ty/src/lib.rs
+++ crates/hir_ty/src/lib.rs
@@ -1,5 +1,10 @@
 use std::collections;
 
+pub fn infer(stmts: Vec<hir::Stmt>, lower_ctx: &hir::BodyLowerContext) -> InferenceResult {
+    let inferencer = TypeInferencer::new();
+    inferencer.infer(stmts, lower_ctx)
+}
+
 #[derive(Debug)]
 pub struct InferenceResult {
     pub type_by_exprs: collections::HashMap<hir::ExprIdx, ResolvedType>,
@@ -33,38 +38,55 @@ pub enum ResolvedType {
     Bool,
 }
 
-pub fn infer_body(stmts: Vec<hir::Stmt>, ctx: &hir::BodyLowerContext) -> InferenceResult {
-    let mut inference_context = InferenceContext::new();
-
-    for stmt in stmts {
-        match stmt {
-            hir::Stmt::Expr(expr) => {
-                let ty = infer_expr(&ctx.exprs[expr]);
-                inference_context.type_by_exprs.insert(expr, ty);
-            }
-            hir::Stmt::VariableDef { value, .. } => {
-                let ty = infer_expr(&ctx.exprs[value]);
-                inference_context.type_by_exprs.insert(value, ty);
-            }
-            hir::Stmt::FunctionDef { .. } => unimplemented!(),
+struct TypeInferencer {
+    ctx: InferenceContext,
+}
+impl TypeInferencer {
+    fn new() -> Self {
+        Self {
+            ctx: InferenceContext::new(),
         }
     }
 
-    InferenceResult {
-        type_by_exprs: inference_context.type_by_exprs,
-        errors: inference_context.errors,
-    }
-}
+    pub fn infer(
+        mut self,
+        stmts: Vec<hir::Stmt>,
+        lower_ctx: &hir::BodyLowerContext,
+    ) -> InferenceResult {
+        self.infer_body(stmts, lower_ctx);
 
-fn infer_expr(expr: &hir::Expr) -> ResolvedType {
-    match expr {
-        hir::Expr::Literal(literal) => match literal {
-            hir::Literal::Integer(_) => ResolvedType::Integer,
-            hir::Literal::String(_) => ResolvedType::String,
-            hir::Literal::Char(_) => ResolvedType::Char,
-            hir::Literal::Bool(_) => ResolvedType::Bool,
-        },
-        _ => todo!(),
+        InferenceResult {
+            type_by_exprs: self.ctx.type_by_exprs,
+            errors: self.ctx.errors,
+        }
+    }
+
+    pub fn infer_body(&mut self, stmts: Vec<hir::Stmt>, ctx: &hir::BodyLowerContext) {
+        for stmt in stmts {
+            match stmt {
+                hir::Stmt::Expr(expr) => {
+                    let ty = self.infer_expr(&ctx.exprs[expr]);
+                    self.ctx.type_by_exprs.insert(expr, ty);
+                }
+                hir::Stmt::VariableDef { value, .. } => {
+                    let ty = self.infer_expr(&ctx.exprs[value]);
+                    self.ctx.type_by_exprs.insert(value, ty);
+                }
+                hir::Stmt::FunctionDef { .. } => unimplemented!(),
+            }
+        }
+    }
+
+    fn infer_expr(&mut self, expr: &hir::Expr) -> ResolvedType {
+        match expr {
+            hir::Expr::Literal(literal) => match literal {
+                hir::Literal::Integer(_) => ResolvedType::Integer,
+                hir::Literal::String(_) => ResolvedType::String,
+                hir::Literal::Char(_) => ResolvedType::Char,
+                hir::Literal::Bool(_) => ResolvedType::Bool,
+            },
+            _ => todo!(),
+        }
     }
 }
 
@@ -79,7 +101,8 @@ mod tests {
         let parsed = parser::parse(input);
         let ast = ast::SourceFile::cast(parsed.syntax()).unwrap();
         let lower_result = hir::lower(ast);
-        let result = infer_body(lower_result.stmts, &lower_result.root_ctx);
+        let inferencer = TypeInferencer::new();
+        let result = inferencer.infer(lower_result.stmts, &lower_result.root_ctx);
         expect.assert_eq(&debug(&result));
     }
 

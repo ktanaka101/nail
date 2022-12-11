4f8d8071d61e4319e3a90501e4e77007555c702d
ktanaka101 <kentanaka101@gmail.com>
2022-12-11 10:38:26 +0900

    wip

---
+++ crates/codegen_llvm/src/lib.rs
@@ -0,0 +1,56 @@
+use inkwell::{
+    basic_block::BasicBlock,
+    builder::Builder,
+    context::Context,
+    execution_engine::{ExecutionEngine, JitFunction},
+    module::Module,
+};
+
+pub fn codegen<'a, 'ctx>(
+    hir_result: &'a hir::LowerResult,
+    ty_result: &'a hir_ty::TyLowerResult,
+    context: &'ctx Context,
+    module: &'a Module<'ctx>,
+    builder: &'a Builder<'ctx>,
+    execution_engine: &'a ExecutionEngine<'ctx>,
+) -> CodegenResult<'ctx> {
+    let codegen = Codegen {
+        hir_result,
+        ty_result,
+        context,
+        module,
+        builder,
+        execution_engine,
+    };
+    codegen.gen()
+}
+
+type MainFunc = unsafe extern "C" fn() -> *mut i8;
+pub struct CodegenResult<'ctx> {
+    function: JitFunction<'ctx, MainFunc>,
+}
+
+struct Codegen<'a, 'ctx> {
+    hir_result: &'a hir::LowerResult,
+    ty_result: &'a hir_ty::TyLowerResult,
+
+    context: &'ctx Context,
+    module: &'a Module<'ctx>,
+    builder: &'a Builder<'ctx>,
+    execution_engine: &'a ExecutionEngine<'ctx>,
+}
+
+impl<'a, 'ctx> Codegen<'a, 'ctx> {
+    fn gen(mut self) -> CodegenResult<'ctx> {
+        let entry_point = self.entry_point();
+        self.builder.position_at_end(entry_point);
+
+        todo!()
+    }
+
+    fn entry_point(&self) -> BasicBlock {
+        let fn_type = self.context.void_type().fn_type(&[], false);
+        let main_fn = self.module.add_function("main", fn_type, None);
+        self.context.append_basic_block(main_fn, "entry")
+    }
+}

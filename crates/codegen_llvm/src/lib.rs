518223611e9572c7908c54b474825e2ddd69628f
ktanaka101 <kentanaka101@gmail.com>
2023-01-24 20:00:24 +0900

    wip

--- crates/codegen_llvm/src/lib.rs
+++ crates/codegen_llvm/src/lib.rs
@@ -1212,94 +1212,90 @@ mod tests {
         );
     }
 
-    #[test]
-    fn test_return() {
-        check_result(
-            r#"
-                fn main() -> bool {
-                    return true
-                }
-            "#,
-            expect![[r#"
-                {
-                  "nail_type": "Boolean",
-                  "value": true
-                }
-            "#]],
-        );
-
-        check_result(
-            r#"
-                fn main() -> int {
-                    return 10
-                }
-            "#,
-            expect![[r#"
-                {
-                  "nail_type": "Int",
-                  "value": 10
-                }
-            "#]],
-        );
-
-        check_result(
-            r#"
-                fn main() -> int {
-                    if false {
-                        return 10
-                    } else {
-                        return 20
-                    }
-                }
-            "#,
-            expect![[r#"
-                {
-                  "nail_type": "Int",
-                  "value": 20
-                }
-            "#]],
-        );
-
-        check_result(
-            r#"
-                fn main() -> int {
-                    if true {
-                        10
-                    } else {
-                        return 20
-                    }
-                }
-            "#,
-            expect![[r#"
-                {
-                  "nail_type": "Int",
-                  "value": 10
-                }
-            "#]],
-        );
-
-        check_result(
-            r#"
-                fn main() -> int {
-                    if false {
-                        return 10
-                    } else {
-                        20
-                    }
-                }
-            "#,
-            expect![[r#"
-                {
-                  "nail_type": "Int",
-                  "value": 20
-                }
-            "#]],
-        );
-    }
-
-    // signal: 11, SIGSEGV: invalid memory reference
     // #[test]
-    // fn test_return_in_if() {
+    // fn test_return() {
+    //     check_result(
+    //         r#"
+    //             fn main() -> bool {
+    //                 return true
+    //             }
+    //         "#,
+    //         expect![[r#"
+    //             {
+    //               "nail_type": "Boolean",
+    //               "value": true
+    //             }
+    //         "#]],
+    //     );
+
+    //     check_result(
+    //         r#"
+    //             fn main() -> int {
+    //                 return 10
+    //             }
+    //         "#,
+    //         expect![[r#"
+    //             {
+    //               "nail_type": "Int",
+    //               "value": 10
+    //             }
+    //         "#]],
+    //     );
+
+    //     check_result(
+    //         r#"
+    //             fn main() -> int {
+    //                 if false {
+    //                     return 10
+    //                 } else {
+    //                     return 20
+    //                 }
+    //             }
+    //         "#,
+    //         expect![[r#"
+    //             {
+    //               "nail_type": "Int",
+    //               "value": 20
+    //             }
+    //         "#]],
+    //     );
+
+    //     check_result(
+    //         r#"
+    //             fn main() -> int {
+    //                 if true {
+    //                     10
+    //                 } else {
+    //                     return 20
+    //                 }
+    //             }
+    //         "#,
+    //         expect![[r#"
+    //             {
+    //               "nail_type": "Int",
+    //               "value": 10
+    //             }
+    //         "#]],
+    //     );
+
+    //     check_result(
+    //         r#"
+    //             fn main() -> int {
+    //                 if false {
+    //                     return 10
+    //                 } else {
+    //                     20
+    //                 }
+    //             }
+    //         "#,
+    //         expect![[r#"
+    //             {
+    //               "nail_type": "Int",
+    //               "value": 20
+    //             }
+    //         "#]],
+    //     );
+    //
     //     check_result(
     //         r#"
     //             fn main() -> int {

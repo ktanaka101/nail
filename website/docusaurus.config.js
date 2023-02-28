1fab4e49ee04ca4cdb9f3fcbc681fd9214868a54
ktanaka101 <kentanaka101@gmail.com>
2023-02-28 18:46:03 +0900

    Update editUrl

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,17 +39,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ktanaka101/nail/tree/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

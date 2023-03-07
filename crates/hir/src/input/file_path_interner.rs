7e4beb0aa9db233a552e194889ec140828fbb4b4
ktanaka101 <kentanaka101@gmail.com>
2023-03-07 21:18:38 +0900

    Allow temporary dead code

--- crates/hir/src/input/file_path_interner.rs
+++ crates/hir/src/input/file_path_interner.rs
@@ -8,10 +8,12 @@ impl FilePathInterner {
         FilePath(self.0.get_or_intern(path))
     }
 
+    #[allow(dead_code)]
     pub(crate) fn get(&self, path: &str) -> Option<FilePath> {
         self.0.get(path).map(FilePath)
     }
 
+    #[allow(dead_code)]
     pub fn lookup(&self, path: FilePath) -> &str {
         self.0.resolve(&path.0)
     }
@@ -20,6 +22,7 @@ impl FilePathInterner {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct FilePath(lasso::Spur);
 impl FilePath {
+    #[allow(dead_code)]
     pub fn lookup(self, interner: &FilePathInterner) -> &str {
         interner.lookup(self)
     }

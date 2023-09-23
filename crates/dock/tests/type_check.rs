286f5ddc59818eb23158007d40c4dbc4416dc2ef
ktanaka101 <kentanaka101@gmail.com>
2023-09-23 19:19:50 +0900

    wip

---
+++ crates/dock/tests/type_check.rs
@@ -0,0 +1,35 @@
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use expect_test::expect_file;
+
+    async fn check(dir_name: &str) {
+        let mut out = Vec::new();
+        let mut err = Vec::new();
+
+        dock::execute(
+            std::env::current_dir()
+                .unwrap()
+                .join(PathBuf::from(&format!(
+                    "tests/type_check/{dir_name}/main.nail"
+                )))
+                .to_str()
+                .unwrap(),
+            &mut out,
+            &mut err,
+        )
+        .await
+        .unwrap_err();
+
+        expect_file![format!("type_check/{dir_name}/main.nail.stdout")]
+            .assert_eq(&String::from_utf8(out).unwrap());
+        expect_file![format!("type_check/{dir_name}/main.nail.stderr")]
+            .assert_eq(&String::from_utf8(err).unwrap());
+    }
+
+    #[tokio::test]
+    async fn return_type() {
+        check("return_type").await;
+    }
+}
